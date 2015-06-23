3b151ded5c8281cec67d3937e2de3c70063fa693
Tom Servo <rachelpage921@gmail.com>
2015-06-22 21:40:12 -0500

    changing Kitties into hangover game

--- hungover_loop.rb
+++ hungover_loop.rb
@@ -1,7 +1,7 @@
 
 
 def prompt
-  print "holla at me bro "
+  print "==> "
 end
 
 class Kitties
@@ -29,15 +29,44 @@ def go(options)
     
 
   else
-    puts "holla one more time"
+    puts "==> "
     go(options)
   end
 end
 
-@options =  {"loki" => Kitties.new("you are fat", false, {"scratch" => Kitties.new("stop it you fucker"), "snuggle" => Kitties.new("who's my lil man")}),
- "grim" => Kitties.new("you are fluffy", true)
-}
-puts @options
+@starting_options = { "look around" => Kitties.new("You’re in your bedroom, sprawled on top of your comforter and pillows. There’s a poster up on the wall of the Star Trek captains and Picard seems to have a distinct look of disapproval on his face as he gazes down at your rather undignified state. Whatever, Jean-Luc, no need to be so snooty about it. \n Next to the bed is a table. On top of the table is a bottle of water. The table also has a drawer, currently closed. The door leading out of the bedroom is to your left.", false, 
+                   { "take water" => Kitties.new("You gulp down the water until the bottle is empty, and say a brief word of thanks to Drunk-you for so helpfully placing it there before passing out last night. Great job, Drunk-you!"), 
+                     "snuggle" => Kitties.new("who's my lil man")}),
+              "fuck it" => Kitties.new("crawl back into bed you loser", true),
+              "stand up" => Kitties.new("Whoah there, cowgirl. Don't know if you're ready to be entirely vertical just yet.", false)
+            }
+puts @starting_options
 
-go(@options)
+def start
+  puts "Your head is pounding and what feels like the light of ten thousand suns"
+  puts "is assaulting your brain directly through your eyes."
+  puts '' 
+  puts "Squinting, you manage to roll over on your bed, reaching out to tug the curtain across the window. Ahh, much better. The effort of moving leaves your head spinning and you collapse back on your pillow to reflect on your situation."
+  puts ''
+  puts "The night before had started as just a few drinks out with friends, but had"
+  puts "quickly professed to a somewhat rowdier evening."
+  puts ''
+  puts "As you lay there in bed some of the details start trickling back..."
+  puts ''
+  puts "There had definitely been shots at some point..."
+  puts ''
+  puts "oh God."
+  puts ''
+  puts "So."
+  puts "many."
+  puts "shots." 
+  puts ''
+  puts "The memory of all that tequila turns your stomach and you quickly focus your"
+  puts "attention back to the present. The clock on the wall informs you that it\'s"
+  puts "already 1 p.m. so it\'s probably time to drag your sorry ass out of bed and get on with your day."
+end
+  
+start 
+
+go(@starting_options)
 
