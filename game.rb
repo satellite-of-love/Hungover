95b02a00f3d2a310c55285e679f6128cb7170443
Jessica Kerr <jessitron@outpace.com>
2015-08-17 02:09:18 +0000

    place to add magic

--- game.rb
+++ game.rb
@@ -31,19 +31,19 @@ class Dozer
   end
   def add_options (m)
     @moar = m
-    self 
+    self
   end
-  def only_once 
+  def only_once
     @only_once = true
-    self 
+    self
   end
   def travelling (t)
     @travelling = t
     self
   end
-  def build 
+  def build
     Kitties.new(@prints, @travelling, @moar, @new_stuff, @only_once)
-  end  
+  end
   def you_get (n)
     @new_stuff = [n]
     self
@@ -55,15 +55,22 @@ module Game
   def self.prompt
     print "==> "
   end
+
   def self.modify_options(old_room_options, kitty, next_move, current_room)
     old_options = old_room_options[current_room]
     new_options = old_options.merge(kitty.moar)
     if kitty.only_once
-      new_options.delete(next_move)   
+      new_options.delete(next_move)
     end
-   old_room_options.merge({current_room => new_options}) 
+   old_room_options.merge({current_room => new_options})
   end
-  def self.go(forever_options, room_options, current_room, inventory = [])
+
+  def self.modify_forever_options(old_ones, magic, inventory)
+    new_forever_options = old_ones
+    new_forever_options
+  end
+
+  def self.go(forever_options, room_options, current_room, inventory = [], magic = nil)
     available_options = room_options[current_room].merge forever_options
     prompt
     next_move = gets.chomp.strip
@@ -72,21 +79,22 @@ module Game
     if kitty
       puts kitty.prints.call(inventory)
       new_room = kitty.where_to_be(current_room)
-      if new_room != :quit   
-        go(forever_options, modify_options(room_options, kitty, next_move, current_room), 
+      if new_room != :quit
+        go(modify_forever_options(forever_options, magic, inventory),
+           modify_options(room_options, kitty, next_move, current_room),
            new_room,
-           kitty.stuff(inventory))
+           kitty.stuff(inventory), magic)
       end
     else
       if next_move == "think"
-        puts "what are some of my options?" 
+        puts "what are some of my options?"
         puts available_options.keys.join(", ")
       else
         puts "say what now"
       end
-      go(forever_options, room_options, current_room, inventory)
+      go(forever_options, room_options, current_room, inventory, magic)
     end
   end
 end
 
-  
+
--- hungover.rb
+++ hungover.rb
@@ -1,9 +1,9 @@
 
 require './game.rb'
 
-print_inventory = ->(inventory) do 
-    if inventory.empty? then "you ain't got nothing" 
-    else "you have something:   " +inventory.join(", ") 
+print_inventory = ->(inventory) do
+    if inventory.empty? then "you ain't got nothing"
+    else "you have something:   " +inventory.join(", ")
     end
   end
 
@@ -12,32 +12,32 @@ forever_options = {
       "inventory" => Kitties.new(print_inventory)
     }
 
-@drawer_options = { 
+@drawer_options = {
   "take vibrator" => Kitties.new("Eh, you’re not really in the mood at the moment.", false),
   "take phone"    => Kitties.new("You pick up your phone. Ten Facebook notifications?! Okay, which one of your asshole friends thought it was okay to tag you in so many horribly unflattering pictures? You quickly untag yourself and send your friend a series of angry emojis to express your displeasure. You place the phone back in the drawer.", false),
-  
+
 
   "take weed" => Dozer.new("Yass queen, now the morning is really coming together! Weed is great for a hangover. Sadly, there’s nothing in this room to smoke it with. You’re currently pants-less (no pockets) but you didn’t bother to taking off your sports bra last night so you stuff the baggie in there for safekeeping. Classy.").
   you_get(:weed).
-  only_once.build 
+  only_once.build
 }
 
 
-  
+
 @leave_room =
   Dozer.new("You slowly manuver yourself forward, offering a brief prayer to any booze-friendly deity that might take pity on your current state. \n ('Are you there, Bacchus? It's me, Margaret'). \n You slowly make your way to the door, open it, and lean against the frame while you contemplate your next move. \n To your left is the living room, to your right is the kitchen.").
 travelling(:bedroom_doorway).build
-  
+
 
   @bedroom_doorway =
   {"go back" =>Kitties.new("You backtrack into the bedroom.", :bedroom),
     "go right" =>Kitties.new("You shuffle into the kitchen, feeling like every generic zombie you’ve ever seen on screen. You peek into the fridge. Apple juice! Mmm yes, drink that. Delicious. There’s also a few slices of leftover pizza. Maybe you’d feel better if you ate something?", :kitchen)
   }
-  
 
-@starting_bedroom_options = 
+
+@starting_bedroom_options =
 { "look around" => Dozer.new("You’re in your bedroom, sprawled on top of your comforter and pillows. There’s a poster up on the wall of the Star Trek captains and Picard seems to have a distinct look of disapproval on his face as he gazes down at your rather undignified state. Whatever, Jean-Luc, no need to be so snooty about it. \n Next to the bed is a table. On top of the table is a bottle of water. The table also has a drawer, currently closed. The door leading out of the bedroom is to your left.").
-       add_options({"take water" => 
+       add_options({"take water" =>
                 Dozer.new("You gulp down the water until the bottle is empty, and say a brief word of thanks to Drunk-you for so helpfully placing it there before passing out last night. Great job, Drunk-you!"
                 ).add_options({"stand up" => Kitties.new("Congratulations! You're vertical now. Good job buddy.", false, {"leave room" => @leave_room })}).
                 only_once.build,
@@ -53,14 +53,19 @@ you_get(:papers).
 only_once.build
 }
 
-bedroom_doorway_options = { 
+bedroom_doorway_options = {
   "go kitchen" => Dozer.new("You shuffle into the kitchen, feeling like every generic zombie you’ve ever seen on screen. You peek into the fridge. Apple juice! Mmm yes, drink that. Delicious. There’s also a few slices of leftover pizza. Maybe you’d feel better if you ate something?").
-  travelling(:kitchen).build, 
-  "go back" => Dozer.new("Turning around makes you dizzy"). 
+  travelling(:kitchen).build,
+  "go back" => Dozer.new("Turning around makes you dizzy").
 travelling(:bedroom).build,
-"go living room" => Dozer.new("The living room is small and a little cluttered. The table has a few items scattered across it.").build,
-"look at table" => Dozer.new("Among all the usual table detritus of empty Solo cups and silverware you find some crumpled dollar bills, a few joint-rolling papers and we all say hallelujah.").add_options(@table_options).build
+  "go living room" => Dozer.new("The living room is small and a little cluttered. The table has a few items scattered across it.")
+     .travelling(:living_room).build,
 }
+
+living_room_options =  {
+    "look at table" => Dozer.new("Among all the usual table detritus of empty Solo cups and silverware you find some crumpled dollar bills, a few joint-rolling papers and we all say hallelujah.").add_options(@table_options).build,
+    "go back" => Dozer.new("Walking backward makes you only a little dizzy.").travelling(:bedroom_doorway).build}
+
 
 kitchen_options = {"eat pizza" =>
 Dozer.new("Oh no. Ohhhh no. Bad idea. You really should have done something about the nausea before attempting food. Now you’re prostate on the kitchen floor, willing your stomach to obey you. Your cat chooses this moment to come paw at your face like the little asshole he is. Eventually you’re able to stand again. Better not try that again until you've gotten your stomach under control").build,
@@ -70,14 +75,15 @@ travelling(:bedroom_doorway).build}
 @starting_options = {
   :bedroom => @starting_bedroom_options,
   :bedroom_doorway => bedroom_doorway_options,
-  :kitchen => kitchen_options
+  :kitchen => kitchen_options,
+  :living_room => living_room_options
 }
 
 
 def start
   puts "Your head is pounding and what feels like the light of ten thousand suns"
   puts "is assaulting your brain directly through your eyes."
-  puts '' 
+  puts ''
   puts "Squinting, you manage to roll over on your bed, reaching out to tug the curtain across the window. Ahh, much better. The effort of moving leaves your head spinning and you collapse back on your pillow to reflect on your situation."
   puts ''
   puts "The night before had started as just a few drinks out with friends, but had"
@@ -91,14 +97,14 @@ def start
   puts ''
   puts "So."
   puts "many."
-  puts "shots." 
+  puts "shots."
   puts ''
   puts "The memory of all that tequila turns your stomach and you quickly focus your"
   puts "attention back to the present. The clock on the wall informs you that it\'s"
   puts "already 1 p.m. so it\'s probably time to drag your sorry ass out of bed and get on with your day."
 end
-  
-start 
+
+start
 
 Game.go(forever_options, @starting_options, :bedroom)
 
